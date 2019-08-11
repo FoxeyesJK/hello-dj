09ec2ac0e6a31cc2037283f4fec422e754a6923f
FoxeyesJK <jakej.kwon@gmail.com>
2019-08-10 21:10:22 -0400

    Add initial mongodb connection

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,100 +1,33 @@
-let postId = 1;
+const Post = require('models/post');
 
-const posts = [
-    {
-        id: 1,
-        title: 'Title',
-        body: 'Body'
-    }
-];
-
-exports.write = (ctx) => {
+exports.write = async (ctx) => {
     //Request body can be found from ctx.request.body in REST API
-    const {
-        title,
-        body
-    } = ctx.request.body;
+    const { title, body, tags } = ctx.request.body;
 
-    postId += 1;
+    const post = new Post({
+        title, body, tags
+    });
 
-    const post = { id: postId, title, body };
-    posts.push(post);
-    ctx.body = post;
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch(e) {
+        ctx.throw(e, 500);
+    }
 }
 
 exports.list = (ctx) => {
-    ctx.body = posts;
+
 }
 
 exports.read = (ctx) => {
-    const { id } = ctx.params;
 
-    const post = posts.find(p => p.id.toString() === id);
-
-    if(!post) {
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post exist'
-        };
-        return;
-    }
-
-    ctx.body = post;
 }
 
 exports.remove = (ctx) => {
-    const { id } = ctx.params;
 
-    const index = posts.findIndex(p => p.id.toString() === id);
-
-    if(index === -1){
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post exist'
-        };
-        return;
-    }
-
-    posts.splice(index, 1);
-    ctx.status = 204;
-};
-
-exports.replace = (ctx) => {
-    const { id } = ctx.params;
-
-    const index = posts.findIndex(p => p.id.toString() === id);
-
-    if(index === -1){
-        ctx.status = 404;
-        ctx.body = {
-            message: "No post exist"
-        };
-        return;
-    }
-    
-    posts[index] = {
-        id,
-        ...ctx.request.body
-    };
-    ctx.body = posts[index];
 }
 
 exports.update = (ctx) => {
-    const { id } = ctx.params;
 
-    const index = posts.findIndex(p => p.id.toString() === id);
-
-    if(index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post exist'
-        };
-        return;
-    }
-
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body
-    };
-    ctx.body = posts[index];
 }
